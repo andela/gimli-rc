696235e3b74a6c1cbf4588266c387e63f4dc5c5b
Esther Falayi <estherfalayi@Esther-Falayi.local>
2017-07-21 10:45:58 +0100

    feat(147529423): redesign header - edit header template from reaction commerce - edit language data - implement new footer design

--- imports/plugins/custom/gimli/client/index.js
+++ imports/plugins/custom/gimli/client/index.js
@@ -1,5 +1,6 @@
 import "./defaults";
 import "./styles/header.less";
+import "./styles/footer";
 import "./styles/bootstrap.less";
 import "./components/navbar/navbar.html";
 import "./components/navbar/navbar";
@@ -7,5 +8,6 @@ import "./components/tagNav/tagNav.html";
 import "./components/tagNav/tagNav";
 import "./templates/layout/header/header.html";
 import "./templates/layout/header/header";
-
+import "./templates/layout/footer/footer.html";
+import "./templates/layout/footer/footer";
 
